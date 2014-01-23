bf08548cbdb9b44d99e31e94673ca3b0b134c03d
Mark Warren <mwarren@kobo.com>
2014-01-22 22:07:55 -0500

    Updated bash script to support command line arguments to start, stop and display status of PlexConnect

--- PlexConnectDaemonHelper.bash
+++ PlexConnectDaemonHelper.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+my_script_version="1.0.2"
 my_script_name=`basename $0 | cut -d'.' -f1`
 
-	clear
+path_to_PlexConnect_Log_File="/Applications/PlexConnect-master/PlexConnect.log"
 
-	echo ""
-	echo "---- $my_script_name v1.0.1 ----"
-	echo ""
+
+function the_main_code()
+{
 
 	echo ""
 	echo "You will be prompted to enter a password for an Administrator user."
@@ -18,12 +18,7 @@ my_script_name=`basename $0 | cut -d'.' -f1`
 	if [ "$current_plexconnect_status" != "" ]
 	then
 		# Plex Connect Daemon is running.
-		echo "PlexConnect is running as:"
-		echo "$current_plexconnect_status"
-		echo ""
-		echo "Status:"
-		echo ""
-		head -25 /Applications/PlexConnect-master/PlexConnect.log
+		plexconnect_daemon_status
 		echo ""
 		echo ""
 		echo "Please select an option. Pressing return will exit"
@@ -34,8 +29,7 @@ my_script_name=`basename $0 | cut -d'.' -f1`
 		echo ""
 		read opt
 		case $opt in
-		1) sudo launchctl unload /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
-		echo "Stopping PlexConnect Daemon";;
+		1) stop_plexconnect_daemon;;
 		2) exit 1;;
 		*) exit 1;;
 		esac
@@ -43,7 +37,7 @@ my_script_name=`basename $0 | cut -d'.' -f1`
 	else
 
 		# Plex Connect Daemon is not running.
-		echo "PlexConnect is not running:"
+		plexconnect_daemon_status
 		echo ""
 		echo "Please select an option. Pressing return will exit"
 		echo "You may be prompted to enter and Administrator User password"
@@ -53,37 +47,156 @@ my_script_name=`basename $0 | cut -d'.' -f1`
 		echo ""
 		read opt
 		case $opt in
-		1) sudo launchctl load /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
-		echo "Starting PlexConnect Daemon";;
+		1) start_plexconnect_daemon;;
 		2) exit 1;;
 		*) exit 1;;
 		esac
 
 
 	fi
-
+	
 	echo ""
-	new_plexconnect_status=`sudo launchctl list | grep plexconnect`
-	if [ "$new_plexconnect_status" != "" ]
+}
+
+
+function start_plexconnect_daemon()
+{
+
+	start_plexconnect_status=`sudo launchctl list | grep plexconnect`
+	if [ "$start_plexconnect_status" != "" ]
 	then
+	
+		plexconnect_daemon_status
+		
+	else
+		sudo launchctl load /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
+		echo "Starting PlexConnect Daemon"
+		echo ""
+		new_plexconnect_status=`sudo launchctl list | grep plexconnect`
+		if [ "$new_plexconnect_status" != "" ]
+		then
 
-		# wait 10 seconds
-		echo "Waiting 5 seconds..."
-		sleep 5
+			# wait 5 seconds
+			echo "Waiting 5 seconds..."
+			sleep 5
 
-		echo "PlexConnect has been started as:"
-		echo "$new_plexconnect_status"
+			echo "PlexConnect has been started as:"
+			echo "$new_plexconnect_status"
+			echo ""
+			echo "Status:"
+			echo ""
+			head -25 "$path_to_PlexConnect_Log_File"
+			echo ""
+
+		else
+
+			echo "ERROR! PlexConnect could not start"
+
+		fi
+		
+	fi
+
+}
+
+
+	
+function stop_plexconnect_daemon()
+{
+
+	
+	stop_plexconnect_status=`sudo launchctl list | grep plexconnect`
+	if [ "$stop_plexconnect_status" != "" ]
+	then
+		sudo launchctl unload /Library/LaunchDaemons/com.plex.plexconnect.bash.plist
+		echo "Stopping PlexConnect Daemon"
+
+		echo ""
+		new_plexconnect_status=`sudo launchctl list | grep plexconnect`
+		if [ "$new_plexconnect_status" != "" ]
+		then
+
+			# wait 5 seconds
+			echo "Waiting 5 seconds..."
+			sleep 5
+
+			echo "PlexConnect has been started as:"
+			echo "$new_plexconnect_status"
+			echo ""
+			echo "Status:"
+			echo ""
+			head -25 "$path_to_PlexConnect_Log_File"
+			echo ""
+
+		else
+
+			echo "PlexConnect has been stopped"
+
+		fi
+
+		echo ""
+		
+	else
+	echo ""
+	echo "PlexConnect could not be stopped as it is not running"
+	echo ""
+	
+	fi
+}
+
+
+function plexconnect_daemon_status()
+{
+	current_plexconnect_status=`sudo launchctl list | grep plexconnect`
+	if [ "$current_plexconnect_status" != "" ]
+	then
+		# Plex Connect Daemon is running.
+		echo ""
+		echo "PlexConnect is running as:"
+		echo "$current_plexconnect_status"
 		echo ""
 		echo "Status:"
 		echo ""
-		head -25 /Applications/PlexConnect-master/PlexConnect.log
-		echo ""
+		head -25 "$path_to_PlexConnect_Log_File"
 
 	else
 
-		echo "PlexConnect has been stopped"
-
+		# Plex Connect Daemon is not running.
+		echo ""
+		echo "PlexConnect is not running:"
+		echo ""
 	fi
+}
 
-	echo ""
 
+
+# Check if the script has been called with any parameters
+clear
+
+echo ""
+echo "---- $my_script_name v$my_script_version ----"
+echo ""
+
+
+if [ "$1" == "start" ]
+then
+	start_plexconnect_daemon
+
+elif [ "$1" == "stop" ]
+then
+	stop_plexconnect_daemon
+
+elif [ "$1" == "status" ]
+then
+
+	plexconnect_daemon_status
+
+elif [ "$1" == "" ]
+then
+
+	the_main_code
+else
+exit 1
+
+fi
+
+echo ""
--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Clone the repository, or download the zip file and expand it.
 I usually copy the PlexConnectDaemonHelper.bash file into /usr/bin/ so it is accessible from anywhere on the command line
 You will also need to use the 'sudo chmod +x PlexConnectDaemonHelper.bash' command to provide execute permissions to the file.
 
-Usage:
-The helper script assumes that PlexConnect is located within a folder named PlexConnect-master which is within the Applications folder. 
+Requirements:
+The helper script assumes that PlexConnect is located within a folder named PlexConnect-master which is within the Applications folder. This is because it displays a portion of the PlexConnect.log file. You can modify the script to change the path to this folder if you wish.
 
-Issue the command "PlexConnectDaemonHelper.bash" from the Terminal. You will be prompted to enter a local administrator users' password, this will typically be the password for the account you are currently using.
+Usage:
+If you simply envoke the helper script with it's name; "PlexConnectDaemonHelper.bash" then it will start up in interactive mode
 
 If the PlexConnect Daemon is running then it will display the process number of the running instance. It will also show a brief status of PlexConnect.
 
@@ -25,3 +26,13 @@ If the PlexConnect Daemon is not running then it will present two options
 
 Pressing 1 will start PlexConnect, you will also see a brief status indication
 Pressing 2 will exit the script without making any changes
+
+The script also supports you supplying command line arguments to it, they are; start, stop, and status.
+
+If you issue the command "PlexConnectDaemonHelper.bash status" then the staus of the PlexConnect Daemon will be returned
+
+If you issue the command "PlexConnectDaemonHelper.bash stop" then the PlexConnect Daemon will be stopped if it is running.
+
+If you issue the command "PlexConnectDaemonHelper.bash start" then the PlexConnect Daemon will be started if it is not running.
+
+Tested on OS X 10.9.1
